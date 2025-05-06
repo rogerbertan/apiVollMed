6fb4deeba99e3cad343885f8497526af0fa0e774
rogerbertan <roger.bertan@gmail.com>
2025-05-06 00:54:47 -0300

    PUT method implemented

---
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.EnderecoDTO;
+
+public record DadosAtualizacaoMedico(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        EnderecoDTO endereco
+) {
+}
