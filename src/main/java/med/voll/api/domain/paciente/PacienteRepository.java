0168b56d620a6301756db959df00fe466d184d22
rogerbertan <roger.bertan@gmail.com>
2025-07-03 17:00:00 -0300

    fix: id query with syntax error

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,6 +1,5 @@
 package med.voll.api.domain.paciente;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,5 +14,5 @@ public interface PacienteRepository extends JpaRepository<Paciente, Long> {
             where
             p.id = :id
             """)
-    Boolean findAtivoById(Long idPaciente);
+    Boolean findAtivoById(Long id);
 }
