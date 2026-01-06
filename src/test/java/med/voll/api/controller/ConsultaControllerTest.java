1dc74b2ea47c55d1ad0cc0173eff4aab6dea8ce3
Roger Bertan <roger.bertan@gmail.com>
2026-01-05 23:12:00 -0300

    fix: replace deprecated lib

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -11,10 +11,10 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -38,7 +38,7 @@ class ConsultaControllerTest {
     @Autowired
     private JacksonTester<DadosDetalhamentoConsulta> jsonDadosDetalhamentoConsulta;
 
-    @MockBean
+    @MockitoBean
     private AgendaDeConsultas agendaDeConsultas;
 
     @Test
